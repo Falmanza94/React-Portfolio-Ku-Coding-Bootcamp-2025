19c1a8b4ce7081d8f823c961e710de3b689b6fa4
Falmanza94 <fischer.almanza@gmail.com>
2025-03-19 10:53:36 -0500

    added handleInput for three components

--- React-Portfolio/src/components/Contact/index.jsx
+++ React-Portfolio/src/components/Contact/index.jsx
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 // import from the css file
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom'; is this still needed?**
 
 function Contact() {
     const [email, setEmail] = useState('');
-    const
+    const [username, setUserName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputType);
+        } else if (inputType === 'userName') {
+            setUserName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
 }
+
