809dbff33bdf1c640e714f471d2e24ea0f591686
Falmanza94 <fischer.almanza@gmail.com>
2025-03-19 10:44:23 -0500

    changed folder/file structure

---
+++ React-Portfolio/src/components/Contact/index.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+// import from the css file
+import { Link } from 'react-router-dom';
+
+function Contact() {
+    const [email, setEmail] = useState('');
+    const
+}
