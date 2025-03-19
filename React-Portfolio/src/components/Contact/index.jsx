7359dffce91372f2942da564c8c0a9846c75618b
Falmanza94 <fischer.almanza@gmail.com>
2025-03-19 11:20:14 -0500

    contact page has more event handlers

--- React-Portfolio/src/components/Contact/index.jsx
+++ React-Portfolio/src/components/Contact/index.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 // import from the css file
 // import { Link } from 'react-router-dom'; is this still needed?**
 
+// Here the state variables for each field in the form are set -- each value is set to an empty string
 function Contact() {
     const [email, setEmail] = useState('');
-    const [username, setUserName] = useState('');
+    const [userName, setUserName] = useState('');
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
@@ -13,6 +14,7 @@ function Contact() {
         const inputType = target.name;
         const inputValue = target.value;
 
+        // Depending on the input type, either email, username, and message is set
         if (inputType === 'email') {
             setEmail(inputType);
         } else if (inputType === 'userName') {
@@ -21,6 +23,51 @@ function Contact() {
             setMessage(inputValue);
         }
     };
+
+    // Prevents the default behavior of the webpage refreshing after form submittal
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        // This addresses whether or not the email is valid -- otherwise an error occurs
+        if (!validateEmail(email) || !userName) {
+            setErrorMessage('Email or Name is invalid');
+            return;
+        }
+        if (!setMessage(message)) {
+            setErrorMessage(`Message is required.`);
+            return;
+        }
+        // Once submission is done correctly, the inputs are cleared
+        setUserName('');
+        setMessage('');
+        setEmail('');
+    };
+
+    return (
+        <section id='contact-me' className='contact'>
+            <div className='flex-row'>
+                <h2 className='section-title secondary-border'>Contact Me</h2>
+            </div>
+
+            <div className='contact-info'>
+                <div>
+                    <h3>Hello {userName}</h3>
+                    <p>Would you like to get in contact?</p>
+                    E:{''}
+                    <a href='mailto://fischer.almanza@gmail.com'>
+                        fischer.almanza@gmail.com
+                    </a>
+                    <p>
+                        <strong>Any and all feedback is most welcome!</strong>
+                    </p>
+                </div>
+                {/* contact form is handled now */}
+                <div className='contact-form'>
+                    <h3>.................</h3>
+                </div>
+            </div>
+        </section>
+    )
 
 }
 
