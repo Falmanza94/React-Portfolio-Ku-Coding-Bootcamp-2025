f5fbe000f68f7629d151ebcce2798af023f9ef6f
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 14:24:31 -0500

    index.jsx imports and function initiated

---
+++ React-Portfolio/src/components/Navigation/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+// #link is used due to this project being a single-page application - scrolling to sections on same page
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to='#aboutme'>About Me</Link></li>
+                <li><Link to='#portfolio'>Portfolio</Link></li>
+                <li><Link to='#resume'>Resume</Link></li>
+                <li><Link to='#contact'>Contact</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
