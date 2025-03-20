d3aac5b42a4f0f356bbaa1b18ea40ad13b8c410b
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 18:01:27 -0500

    changed pathways for links

--- React-Portfolio/src/components/Navigation/index.jsx
+++ React-Portfolio/src/components/Navigation/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // #link is used due to this project being a single-page application - scrolling to sections on same page
 function Navigation() {
     return (
         <nav>
             <ul>
-                <li><a href='#aboutme'>About Me</a></li>
-                <li><a href='#portfolio'>Portfolio</a></li>
-                <li><a href='#resume'>Resume</a></li>
-                <li><a href='#contact'>Contact</a></li>
+                <li><Link to='/aboutme'>About Me</Link></li>
+                <li><Link to='/portfolio'>Portfolio</Link></li>
+                <li><Link to='/resume'>Resume</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
             </ul>
         </nav>
     );
