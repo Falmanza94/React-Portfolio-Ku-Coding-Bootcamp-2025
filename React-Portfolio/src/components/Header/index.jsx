1cde034c502b2a6358fb007230cc939981d68c0f
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 14:15:59 -0500

    basic Header.jsx initiated

---
+++ React-Portfolio/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Fischer Almanza</h1>
+            <p>Student Web Developer</p>
+        </header>
+    );
+}
+
+export default Header;
