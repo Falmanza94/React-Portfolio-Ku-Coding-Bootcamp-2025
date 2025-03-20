0767ad05c6352eb366243b7240e6cd68bec39154
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 18:31:15 -0500

    import and image links added

--- React-Portfolio/src/pages/Portfolio/index.jsx
+++ React-Portfolio/src/pages/Portfolio/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Project from '../../components/Project/index.jsx';
+import ledWallImage from '../../assets/images/led_wall.avif';
 
-const Portfolio = () => {
-  return <div>Portfolio Page</div>;
-};
+function Portfolio() {
+  return (
+    <section id='portfolio'>
+      <h2>Portfolio</h2>
+      <div className='project-list'>
+        <Project
+          title='Led Wall'
+          image={ledWallImage}
+        />
+        </div>
+    </section>
+  )
+} 
 
 export default Portfolio;
+
+// I need to add images for my projects
+// I also need to see what type of images I need
