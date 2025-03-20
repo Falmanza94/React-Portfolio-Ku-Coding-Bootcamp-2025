f4e3d5c1c8ef13254690d4ec591a156bcf04a501
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 14:55:03 -0500

    AboutMe imports and function initiated

--- React-Portfolio/src/pages/AboutMe/index.jsx
+++ React-Portfolio/src/pages/AboutMe/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import portrait_photo from '../../assets/images/portrait_photo.jpg';
-function About() {
+import React from 'react';
+import portrait from '../../assets/images/portrait.jpg';
+
+function AboutMe() {
     return (
-        <section id='about-me' className=''
-    )
+        <section id='aboutme'>
+            <img src={portrait} alt='Portrait of the developer'/>
+            <p>Hello! I am a student web developer interested in building websites, applications and eventually video games. Whenever I see a well desinged website or application I think, "Hey I want to know how it works." Or "I bet I could make these colors pop more!" My hope is that some day I can take what I have learned from web development and design and apply that to a future video game.</p>
+        </section>
+    );
 }
+
+export default AboutMe;
