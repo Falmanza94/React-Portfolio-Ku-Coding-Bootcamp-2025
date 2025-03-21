901733eda5748cf72d7f5f572e1971e88f78815a
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 22:29:52 -0500

    added const to refer to my Nav function aboutMeRef and altered paths for imports

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
+import Header from './components/Header/index.jsx';
+import Navigation from './components/Navigation/index.jsx';
+import Footer from './components/Footer/index.jsx';
+import AboutMe from './pages/AboutMe/index.jsx';
+import Portfolio from './pages/Portfolio/index.jsx';
+import Resume from './pages/Resume/index.jsx';
+import Contact from './pages/Contact/index.jsx';
 import './styles/index.css';
 
 function App() {
+  const aboutMeRef = React.useRef(null);
+
   return (
     <div className='App'>
       <Header />
-      <Navigation />
+      <Navigation aboutMeRef={aboutMeRef} />
       <div id='main-content'>
         <Routes>
           <Route path='/' element={<AboutMe />} />
