458e158f8fcc0879a617beef000f44b256fffccd
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 21:38:41 -0500

    added some inline css for the profile pic

--- React-Portfolio/src/pages/AboutMe/index.jsx
+++ React-Portfolio/src/pages/AboutMe/index.jsx
@@ -4,7 +4,7 @@ import portrait from '../../assets/images/portrait.jpg';
 function AboutMe() {
     return (
         <section id='aboutme'>
-            <img src={portrait} alt='Portrait of the developer'/>
+            <img src={portrait} alt='Portrait of the developer' style={{ width: '200px', height: 'auto' }} />
             <p>Hello! I am a student web developer interested in building websites, applications and eventually video games. Whenever I see a well desinged website or application I think, "Hey I want to know how it works." Or "I bet I could make these colors pop more!" My hope is that some day I can take what I have learned from web development and design and apply that to a future video game.</p>
         </section>
     );
