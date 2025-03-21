743d698d64c02241df6edc81982e6c2f2e4c6974
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 21:31:45 -0500

    changed p to h2

--- React-Portfolio/src/components/Header/index.jsx
+++ React-Portfolio/src/components/Header/index.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header>
             <h1>Fischer Almanza</h1>
-            <p>Student Web Developer</p>
+            <h2><p>Student Web Developer</p></h2>
         </header>
     );
 }
