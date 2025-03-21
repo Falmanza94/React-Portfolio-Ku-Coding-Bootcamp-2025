d428d4283d5bc5848075a485500afc49c166c611
Falmanza94 <fischer.almanza@gmail.com>
2025-03-20 20:40:58 -0500

    added placeholder images for projects

--- React-Portfolio/src/pages/Portfolio/index.jsx
+++ React-Portfolio/src/pages/Portfolio/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Project from '../../components/Project/index.jsx';
 import ledWallImage from '../../assets/images/led_wall.avif';
+import pastelPuzzle from '../../assets/images/pastel_puzzle.avif';
+import reactCalc from '../../assets/images/reac_calc.avif';
+import runBuddy from '../../assets/images/run_buddy.avif';
+import surfReport from '../../assets/images/surf_report.avif';
 
 function Portfolio() {
   return (
@@ -10,6 +14,22 @@ function Portfolio() {
         <Project
           title='Led Wall'
           image={ledWallImage}
+        />
+         <Project
+          title='Pastel Puzzle'
+          image={pastelPuzzle}
+        />
+         <Project
+          title='React Calculator'
+          image={reactCalc}
+        />
+        <Project
+          title='Run Buddy'
+          image={runBuddy}
+        />
+        <Project
+          title='Surf Report'
+          image={surfReport}
         />
         </div>
     </section>
